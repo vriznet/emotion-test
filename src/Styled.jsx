fc8ef68510bc0f3ee0cb8ec0891fa6a725ceffae
play.js <git@gituser.com>
2021-04-30 22:25:30 +0900

    emotion setting

---
+++ src/Styled.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  color: ${props => props.primary ? 'hotpink' : 'turquoise'};
+`;
+
+const WithButton = Button.withComponent('button');
+
+const ParentOfButton = styled.div`
+  ${Button} {
+    color: 'green';
+  }
+`;
+
+const Container = styled.div(props => ({
+  display: 'flex',
+  flexDirection: props.column && 'column'
+}));
+
+const Styled = () => {
+  return (
+    <>
+      <Container column>
+        <Button>This is a regular button</Button>
+        <Button primary>This is a primary button</Button>
+        <WithButton primary>This is a with primary button</WithButton>
+        <ParentOfButton>
+          <Button>This is a child Button</Button>
+        </ParentOfButton>
+      </Container>
+    </>
+  )
+};
+
+export default Styled;
